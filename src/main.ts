f07a5cad826d6af73a23682ddde8b5859cba84a3
HAX <adrianelizandro78@gmail.com>
2025-06-08 11:38:28 -0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new PrismaExceptionFilter());
+  app.enableCors();
   const port = 3000
   await app.listen(port);
   console.log(`Listen at: http://localhost:${port}`)
-
 }
 bootstrap();
